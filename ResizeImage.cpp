e4957c9c2cf5b45003621c01c1dc4e9dbee17a95
VladaZhmurkevych <31669221+VladaZhmurkevych@users.noreply.github.com>
2018-05-19 10:37:50 +0300

    Update ResizeImage.cpp

--- ResizeImage.cpp
+++ ResizeImage.cpp
@@ -81,6 +81,42 @@ int findPadding(int width) {
 	return padding;
 }
 
+void putColor(char blue, char green, char red) {
+	outputFile.put(blue);
+	outputFile.put(green);
+	outputFile.put(red);
+}
+
+void getColor(char &blue, char &green, char &red) {
+	inputFile.get(blue);
+	inputFile.get(green);
+	inputFile.get(red);
+}
+
+void imageColor() {
+	char blue, green, red;
+	for (int i = 0; i < height; i++) {
+		for (int j = 0; j < resize; j++) {
+			for (int k = 0; k < width; k++) {
+				getColor(blue, green, red);
+				for (int l = 0; l < resize; l++) {
+					putColor(blue, green, red);
+				}
+			}
+			int oldPadding, newPadding;
+			oldPadding = findPadding(width);
+			newPadding = findPadding(newWidth);
+			for (int k = 0; k < oldPadding; k++) {
+				inputFile.get(ch);
+			}
+			for (int k = 0; k < newPadding; k++) {
+				outputFile.put(ch);
+			}
+			inputFile.seekg(54 + i * (width * 3 + oldPadding));
+		}
+	}
+}
+
 void readHeader(unsigned char header[54]) {
 	for (int i = 0; i < 18; i++) {
 		ch = header[i];
@@ -88,3 +124,11 @@ void readHeader(unsigned char header[54]) {
 	}
 }
 
+void findOldHeightAndWidth(unsigned char header[54]) {
+	for (int i = 0; i < 4; i++) {
+		ch = header[18 + j];
+		width += ch * pow(256, j);
+		ch = header[22 + j];
+		height += ch * pow(256, j);
+	}
+}
